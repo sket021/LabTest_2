2cc988ded3fb7b075245a55f2361caf2825da0c0
sket021 <31525874+sket021@users.noreply.github.com>
2017-10-26 19:32:09 +1300

    Add files via upload

---
+++ Sketch.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using Pathfinding.Serialization.JsonFx; //make sure you include this using
+
+public class Sketch : MonoBehaviour {
+    public GameObject myPrefab;
+    public string _WebsiteURL = "http://chronospatial.azurewebsites.net/tables/Town?zumo-api-version=2.0.0";
+
+    void Start () {
+        //Reguest.GET can be called passing in your ODATA url as a string in the form:
+        //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
+        //The response produce is a JSON string
+        string jsonResponse = Request.GET(_WebsiteURL);
+
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse))
+        {
+            return;
+        }
+
+        
+        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+        Town[] towns = JsonReader.Deserialize<Town[]>(jsonResponse);
+
+        //----------------------
+        //YOU WILL NEED TO DECLARE SOME VARIABLES HERE SIMILAR TO THE CREATIVE CODING TUTORIAL
+
+        int i = 0;
+        int totalCubes = 5;
+        float totalDistance = 2.9f;
+        //----------------------
+
+        //We can now loop through the array of objects and access each object individually
+        foreach (Town town in towns)
+        {
+            //Example of how to use the object
+            Debug.Log("This products name is: " + town.TownName);
+            //----------------------
+            //YOUR CODE TO INSTANTIATE NEW PREFABS GOES HERE
+            float perc = i / (float)totalCubes;
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
+
+            float latitude = town.Latitude;
+            float altitude = town.Altitude;
+            float longitude = town.Longitude;
+            float Size = town.Size;
+            string symbol = town.Symbol;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(latitude, altitude, longitude), Quaternion.identity);
+            
+            newCube.GetComponent<CubeScript>().SetSize(.45f * (1.0f - perc));
+            newCube.GetComponent<CubeScript>().rotateSpeed = .2f + perc * 4.0f;
+            newCube.transform.Find("New Text").GetComponent<TextMesh>().text = town.TownName;//"Hullo Again";
+            i++;
+            
+
+            //----------------------
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Town.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Town
+{
+    //List all the columns of your table here in the same format as below.
+    public string TownId { get; set; }
+    public string TownName { get; set; }
+    public string Symbol { get; set; }
+    public float Latitude { get; set; }
+    public float Altitude { get; set; }
+    public float Longitude { get; set; }
+    public float Size { get; set; }
+}
